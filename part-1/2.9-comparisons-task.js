43ad675fe0e24bd3b95b498e2863f8964d72d3c1
Bo Yeon Ihn <boyeonihn@gmail.com>
2022-07-24 23:58:03 +0900

    added 2.9 task solution

---
+++ part-1/2.9-comparisons-task.js
@@ -0,0 +1,22 @@
+/* Comparisons
+importance: 5
+What will be the result for these expressions?
+
+5 > 4
+"apple" > "pineapple"
+"2" > "12"
+undefined == null
+undefined === null
+null == "\n0\n"
+null === +"\n0\n"
+*/
+
+// MY SOLUTION
+5 > 4 // true
+"apple" > "pineapple" // false
+"2" > "12" // false  -> actually, it is true because unicode order 
+undefined == null // true 
+undefined === null // false 
+null == "\n0\n" // false 
+null === +"\n0\n" // false 
+*/
