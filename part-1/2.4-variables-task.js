1a07f3874e5495e4aa3a72342c2a145aedeaebb5
Bo Yeon Ihn <boyeonihn@gmail.com>
2022-07-24 09:51:00 +0900

    first commit added 2.4 task

---
+++ part-1/2.4-variables-task.js
@@ -0,0 +1,22 @@
+/* Working with variables
+importance: 2
+Declare two variables: admin and name.
+Assign the value "John" to name.
+Copy the value from name to admin.
+Show the value of admin using alert (must output “John”).
+*/
+
+// MY SOLUTION
+let admin;
+let name; // Declare two variables: admin and name.
+
+name = "John"; // Assign the value "John" to name.
+admin = name; // Copy the value from name to admin.
+
+alert(admin); // Show the value of admin using alert (must output “John”)
+
+
+
+
+
+
